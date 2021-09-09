2f6c4b607935704a71c29300df7d16830720c447
Nevin-Edwin <nevin.e@inapp.com>
2021-09-09 17:18:11 +0530

    Made a change in dictionary indexing method

--- Week_1.py
+++ Week_1.py
@@ -5,8 +5,8 @@ play_round = 1
 (player_point, computer_point) = (0, 0)
 
 while play_round < 10:
-    computer = random.choices(history["choices"])[0]
-    player = input('your turn choose any one of this {}:'.format(history["choices"])).title()
+    computer = random.choices(history.get("choices"))[0]
+    player = input('your turn choose any one of this {}:'.format(history.get("choices"))).title()
     result = str()
 
     if computer == player:
@@ -21,23 +21,24 @@ while play_round < 10:
             computer_point += 1
             result = "Computer won Round {}".format(play_round)
         else:
-            print("Enter the correct input from {}".format(history["choices"]))
+            print("Enter the correct input from {}".format(history.get("choices")))
             continue
 
     history[play_round] = {"computer_choice": computer, "player_choice": player, "result": result,
                            "points": "player point : {}, computer point : {}".format(player_point, computer_point)}
     play_round += 1
 
-
 if player_point == computer_point:
     print("Both get equal points : {}, so game becomes tie.".format(player_point, computer_point))
 else:
     if player_point > computer_point:
-        print("Player gets {} points. \ncomputer gets {} points. \nWinner is Player".format(player_point, computer_point))
+        print(
+            "Player gets {} points. \ncomputer gets {} points. \nWinner is Player".format(player_point, computer_point))
     else:
-        print("Player gets {} points. \ncomputer gets {} points. \nWinner is Computer".format(player_point, computer_point))
+        print("Player gets {} points. \ncomputer gets {} points. \nWinner is Computer".format(player_point,
+                                                                                              computer_point))
 
-
-details = int(input("Enter the round for which you need a information >> "))
-print("Player choice = {} \nComputer choice = {} \n{}".format(history[details]["player_choice"], history[details]["computer_choice"], history[details]["result"]))
-
+details = int(input("Enter the round for which you need the information >> "))
+print("Player choice = {} \nComputer choice = {} \n{}".format(history.get(details).get("player_choice"),
+                                                              history.get(details).get("computer_choice"),
+                                                              history.get(details).get("result")))
