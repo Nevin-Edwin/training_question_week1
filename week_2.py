f3ed88b8c36282dab7ed264f25a055e441a61799
Nevin Edwin <nevin.e@inapp.com>
2021-09-16 18:36:17 +0530

    solve the problem over writing

--- week_2.py
+++ week_2.py
@@ -9,14 +9,14 @@ def screen(choice):
 
 def row_check(val, sym):
     for row in range(0, 7, 3):
-        if val[row] == val[row+1] == val[row+2] == sym:
+        if val[row] == val[row + 1] == val[row + 2] == sym:
             return True
     return False
 
 
 def col_check(val, sym):
     for row in range(0, 3):
-        if val[row] == val[row+3] == val[row+6] == sym:
+        if val[row] == val[row + 3] == val[row + 6] == sym:
             return True
     return False
 
@@ -35,10 +35,10 @@ def compare(valuee, symbol):
 
 
 def play_game():
-    his = [None]*10
+    his = [None] * 10
     flag = 0
     options = ["X", "O"]
-    values = [" "]*9
+    values = [" "] * 9
     sample = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     for i in range(0, 9):
         if i % 2 == 0:
@@ -48,7 +48,7 @@ def play_game():
                     sample.remove(user)
                     values[user] = options[0]
                     screen(values)
-                    his[i] = values
+                    his[i] = values.copy()
                     if compare(values, options[0]):
                         result = "User Won the Game"
                         his[9] = result
@@ -65,14 +65,14 @@ def play_game():
             sample.remove(computer)
             values[computer] = options[1]
             screen(values)
-            his[i] = values
+            his[i] = values.copy()
             if compare(values, options[1]):
                 result = "Computer Won the Game"
                 his[9] = result
                 break
     if i == 8:
         result = "Tie in Game"
-        his[i+1] = result
+        his[i + 1] = result
 
     return his
 
@@ -81,7 +81,8 @@ def play_game():
 game_rounds = 1
 history = {}
 sam = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-while game_rounds <= 10:
+while game_rounds <= 2:
+    print("Game {}:".format(game_rounds))
     screen(sam)
     history[game_rounds] = play_game()
     game_rounds += 1
@@ -89,8 +90,9 @@ while game_rounds <= 10:
 details = int(input("Are you want to see any round :"))
 for key, value in history.items():
     if key == details:
-        for element in range(len(value)-1):
+        for element in range(len(value) - 1):
             if value[element] is not None:
-                print("Move {}".format(element+1))
+                print("\n")
+                print("Move {}".format(element + 1))
                 screen(value[element])
         print(value[-1])
